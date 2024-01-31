58c74f91080c8b697b236cb640165067cc0f7b6d
nebraszka <majusxp@gmail.com>
2024-01-31 23:34:15 +0100

    Add sanitization (XSS attacks)

--- SecureNotes.Blazor/Models/NoteDtos/AddNote.cs
+++ SecureNotes.Blazor/Models/NoteDtos/AddNote.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SecureNotes.API.Models.NoteDtos
 {
     public class AddNoteDto
     {
+        [Required(ErrorMessage = "Tytuł jest wymagany.")]
+        [StringLength(50, ErrorMessage = "Tytuł może mieć maksymalnie 50 znaków.")]
+        [RegularExpression(@"^[a-zA-Z0-9\sąćęłńóśźżĄĆĘŁŃÓŚŹŻ]*$", ErrorMessage = "Tytuł może zawierać tylko litery, cyfry i spacje.")]
         public string Title { get; set; } = string.Empty;
         public string Content { get; set; } = string.Empty;
         public bool IsPublic { get; set; } = false;
