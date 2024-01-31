3c73672e6138dd51eda48e6168e9344354dd48b5
nebraszka <majusxp@gmail.com>
2024-02-01 00:00:08 +0100

    Fix change password method and improve password forms

--- SecureNotes.Blazor/Models/NoteDtos/UpdateNote.cs
+++ SecureNotes.Blazor/Models/NoteDtos/UpdateNote.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SecureNotes.API.Models.NoteDtos
 {
     public class UpdateNoteDto
     {
         public Guid NoteId { get; set; }
+        [Required(ErrorMessage = "Tytuł jest wymagany.")]
+        [StringLength(50, ErrorMessage = "Tytuł może mieć maksymalnie 50 znaków.")]
+        [RegularExpression(@"^[a-zA-Z0-9\sąćęłńóśźżĄĆĘŁŃÓŚŹŻ]*$", ErrorMessage = "Tytuł może zawierać tylko litery, cyfry i spacje.")]
         public string Title { get; set; } = string.Empty;
         public string Content { get; set; } = string.Empty;
         public string? Password { get; set; } = string.Empty;
